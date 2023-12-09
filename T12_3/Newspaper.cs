0971e0e206af39409e093dfc83ca3619030b8cf6
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-12-09 20:33:56 +0200

    Newspaper.cs

--- T12_3/Newspaper.cs
+++ T12_3/Newspaper.cs
@@ -11,19 +11,32 @@ namespace T12_3
     /// </summary>
     class Newspaper
     {
+        
         public string name;
         public int circulation;
         public int lines;
         public Newspaper() { }
-        public Newspaper(string name, int circulation, int lanes) 
-        {
-
-        }
-        public void Info()
-        {
-            Console.WriteLine($"Название: {name};\nТираж: {circulation};\nКоличество полос: {lines};\n");
-        }
-
+         public Newspaper(string name, int circulation, int lanes)
+    {
+     this.name = name;
+     this.circulation = circulation; 
+     this.lanes = lanes;
+    }
+    public void Info()
+    {
+     Console.WriteLine($"Название: {name};\nТираж: {circulation};\nКоличество полос: {lanes};\n");
+    }
+     public void ChangeName(string Newname)
+    {
+   
+     name = Newname;
+     Console.WriteLine($"Название новой газеты: {Newname};");
+    }
+    public void ChangeNumber(int Newlanes)
+    {
+     lanes = Newlanes;
+     Console.WriteLine($"Количество новый полос: {Newlanes};");
+    }
 
     }
 }
